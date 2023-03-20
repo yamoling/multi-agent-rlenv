262780bcf00b9baaa33db10c7e6607d9dfa75a6d
Yannick Molinghen <yannick.molinghen@ulb.be>
2023-03-20 17:46:46 +0100

    touch

--- src/main.py
+++ src/main.py
@@ -4,23 +4,10 @@ import rlenv
 print(rlenv.__version__)
 import rlenv
 # From Gym
-env = rlenv.make("CartPole-v1")
 
-# From pettingzoo
-from pettingzoo.sisl import pursuit_v4
-env = rlenv.make(pursuit_v4.parallel_env())
-
-
-# Building the environment with additional information
-from pettingzoo.sisl import pursuit_v4
-env = rlenv.Builder(pursuit_v4.parallel_env())\
-    .agent_id()\
-    .last_action()\
-    .build()
-
-assert env.extra_feature_shape == (13, )
+env = (rlenv.Builder("CartPole-v1")
+       .agent_id()
+       .pad("extra", 2)
+       .build())
 
 
-test = rlenv.Builder("CartPole-v1").add_logger("action", "logs").build()
-test.reset()
-[test.step([0]) for i in range(20)]
