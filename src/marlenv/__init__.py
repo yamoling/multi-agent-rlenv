4d58905676b97e3d43a7c02fe91c3d40bb0e09fe
Yannick Molinghen <yannick.molinghen@ulb.be>
2024-10-09 22:38:01 +0200

    Version bump

--- src/marlenv/__init__.py
+++ src/marlenv/__init__.py
@@ -9,7 +9,7 @@ It aims to
     - provide helpful wrappers to add intrinsic rewards, agent ids, record videos, ...
 """
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 from . import models
 from . import wrappers
